e817b235a43d4be2ca7b9eb82af63d1bf7fce014
NashMatthews <mwnash@protonmail.com>
2023-02-17 12:38:12 +0000

    First Commit

---
+++ training.py
@@ -0,0 +1,71 @@
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+import tensorflow as tf
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+import random
+import json
+import nltk
+import pickle
+
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('punkt')
+
+lemmatizer = nltk.WordNetLemmatizer()
+
+intents = json.loads(open('data.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = [',', '?', '!', '.', '/', '\'']
+
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+
+words = sorted(set(words))
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pk1', 'wb'))
+pickle.dump(classes, open('classes.pk1', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(len(train_y[0]), activation='softmax'))
+
+sgd = tf.keras.optimizers.legacy.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+history = model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=32, verbose=1)
+model.save('chatbot_model.model', history)
